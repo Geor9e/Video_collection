8a4b1a342cf5ce30231ae2489993f0ee4abf63e8
6e0r9e <1@6e0r9e.com>
2013-06-02 23:59:11 -0400

    Add get path for genre and fix sql commands

--- main.rb
+++ main.rb
@@ -1,9 +1,56 @@
-require 'json'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'active_support/all'
 require 'pg'
 
+
+get('/') {
+  @genre = get_genre
+  erb :home
+}
+
+
+post('/create') {
+  sql = "Insert into videos (title, description, url, genre)
+         values ('#{params['title']}', '#{params['description']}', '#{params['url']}', '#{params['genre'].downcase}')"
+  run_sql(sql)
+  redirect to('/videos')
+}
+
+get('/videos') {
+  sql = 'select * from videos'
+  @rows = run_sql(sql)
+  @genre = get_genre
+  erb :videos
+}
+
+get('/videos/:id/edit') {
+  sql = "select * from videos where id = #{params['id']}"
+  @row = run_sql(sql).first
+  erb :edit
+}
+
+
+get '/videos/genre/:genre' do
+  @genre = get_genre
+  sql = "select * from videos where genre = '#{params['genre']}'"
+  @rows = run_sql(sql)
+  erb :genre
+end
+
+post('/videos/:id/edit') {
+  sql = "update videos set
+         title='#{params['title']}', description='#{params['description']}', url='#{params['url']}', genre='#{params['genre']}'
+         where id = #{params['id']}"
+  run_sql(sql)
+  redirect '/videos'
+}
+
+post('/videos/:id/delete') {
+  sql = "delete from videos where id = #{params['id']}"
+  run_sql(sql)
+  redirect '/videos'
+}
 
 def run_sql(sql)
   conn = PG.connect(dbname: 'video_store', host: 'localhost')
@@ -12,41 +59,8 @@ def run_sql(sql)
   results
 end
 
-get('/') {
-  sql = "select * from videos"
-  @rows = run_sql(sql)
-  erb :home
-}
 
-
-get('/videos') {
-  sql = 'select * from videos'
-  @rows = run_sql(sql)
-  erb :videos
-}
-
-
-post('/create') {
-  sql = "insert into videos (title,description,url,genre)
-           values ('#{params['title']}','#{params['description']}','#{params['url']}','#{params['genre']}')"
-  run_sql(sql)
-  redirect '/videos'
-}
-
-
-post('/videos/:id') {
-  sql ="update videos set title='#{params['title']}', description='#{params['description']}', url='#{params['url']}',
-        genre='#{params['genre']}' WHERE id=#{params['id']}"
-  run_sql(sql)
-  redirect to('/videos')
-}
-
-
-get('/videos/:id/edit') {
-  sql = "update videos set title='#{params['title']}', description='#{params['description']}',
-         url='#{params['url']}', genre='#{params['genre'].downcase}'
-         where id = #{params['id']}"
-  run_sql sql
-  redirect to('/videos')
-}
-
+def get_genre
+  sql = "select distinct genre from videos"
+  genre = run_sql(sql)
+end
